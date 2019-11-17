44d091feafb523fbdd79ae48d9b9c5ced464bcb2
PedroChies <pedrohenrique.cont@hotmail.com>
2019-11-17 13:52:19 -0300

    CompeticaoMultimodalidade

--- CompeticaoMultimodalidades.cpp
+++ CompeticaoMultimodalidades.cpp
@@ -1,9 +1,29 @@
 #include "CompeticaoMultimodalidades.h"
 
-CompeticaoMultimodalidades::CompeticaoMultimodalidades() {
+CompeticaoMultimodalidades::CompeticaoMultimodalidades(string nome, Equipe** equipes, int quantidade): Competicao(nome, equipes, quantidade) {
 
 }
 
 CompeticaoMultimodalidades::~CompeticaoMultimodalidades() {
+
+}
+
+void CompeticaoMultimodalidades::adicionar() {
+
+}
+
+Tabela* CompeticaoMultimodalidades::getTabela() {
+
+}
+
+void CompeticaoMultimodalidades::setPontuacao(vector<int>* pontos) {
+
+}
+
+int CompeticaoMultimodalidades::getPontoPorPosicao(int posicao) {
+
+}
+
+void CompeticaoMultimodalidades::imprimir() {
 
 }
--- CompeticaoMultimodalidades.h
+++ CompeticaoMultimodalidades.h
@@ -3,22 +3,29 @@
 #include "Equipe.h"
 #include "Modalidade.h"
 #include "Competicao.h"
+#include <list>
+#include <vector>
 
 using namespace std;
 
-class CompeticaoMultimodalidades
-{
+class CompeticaoMultimodalidades: public Competicao {
     public:
         CompeticaoMultimodalidades(string nome, Equipe** equipes, int quantidade);
         virtual ~CompeticaoMultimodalidades();
 
         void adicionar(Modalidade* m);
         list<Modalidade*>* getModalidades();
+        Tabela* getTabela();
 
         static void setPontuacao(vector<int>* pontos);
         static int getPontoPorPosicao(int posicao);
+        void imprimir();
 
     private:
+        int quantidadeDeModalidades;
+        string nome;
+        Equipe** equipesParticipantes;
+        list<Modalidade*>* listaDeModalidades;
 };
 
 #endif // COMPETICAOMULTIMODALIDADES_H
